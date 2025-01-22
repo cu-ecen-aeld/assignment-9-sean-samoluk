1c10ec98682b70379e5877d5233335bce0acf40f
Sean Samoluk <seansamoluk@outlook.com>
2025-01-22 00:29:12 -0700

    Remove test code.

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -14,19 +14,5 @@ LDD_GIT_SUBMODULES = YES
 
 LDD_MODULE_SUBDIRS = misc-modules scull
 
-#BR2_ROOTFS_OVERLAY = ../base_external/rootfs_overlay
-
-#define LDD_BUILD_CMDS
-#	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull all
-#	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules all
-#endef
-
-#$(INSTALL) -m 0755 $(@D)/scull/scull.ko ${TARGET_DIR}/lib/modules/$(uname -r)/kernel/ldd
-#$(INSTALL) -m 0755 $(@D)/misc-modules/faulty.ko ${TARGET_DIR}/lib/modules/$(uname -r)/kernel/ldd
-#$(INSTALL) -m 0755 $(@D)/misc-modules/hello.ko ${TARGET_DIR}/lib/modules/$(uname -r)/kernel/ldd
-
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-
-
 $(eval $(kernel-module))
 $(eval $(generic-package))
