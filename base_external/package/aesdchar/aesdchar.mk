a05564bc6f9b195372a2ea8dc7043c8db1f8ef68
Sean Samoluk <seansamoluk@outlook.com>
2025-02-04 22:21:23 -0700

    Assignment 8 implementation.

---
+++ base_external/package/aesdchar/aesdchar.mk
@@ -0,0 +1,24 @@
+##############################################################
+#
+# AESDCHAR
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESDCHAR_VERSION = 8bb3961b34f8584a6beaa5aec2cdca15796d2adb
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-sean-samoluk.git
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
+
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
+
+define AESDCHAR_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
